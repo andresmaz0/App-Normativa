f6a59609b71416365d59e45aec09df2050daff68
andresmaz0 <andresdavidmazo@gmail.com>
2024-10-30 17:18:35 -0600

    añadiento prevent

--- Js/Medidas.js
+++ Js/Medidas.js
@@ -2,13 +2,19 @@ const form = document.getElementById("form");
 
 let mensaje = document.getElementById("mensaje");
 
-let validacion = () => {
+let validacion = (e) => {
+    e.preventDefault();
     let ancho = document.getElementById("ancho").value;
     let relacion = document.getElementById("relacion").value;
     let rin = document.getElementById("rin").value;
+    console.log(ancho, relacion, rin);
+    if(ancho>1){
+        mensaje.value='SE RECOMIENDA CAMBIAR DE MUESTRA';
+        console.log("ay");
+    }
 
-    mensaje.value='SE RECOMIENDA CAMBIAR DE MUESTRA';
-    mensaje.value='LA MUESTRA ES APTA PARA ESTE ENSAYO';
+    //mensaje.value='SE RECOMIENDA CAMBIAR DE MUESTRA';
+    //mensaje.value='LA MUESTRA ES APTA PARA ESTE ENSAYO';
 }
 
 form.addEventListener("submit", validacion);
