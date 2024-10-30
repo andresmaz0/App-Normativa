c18b599c05df380e37136aceb5c6afd0995d746b
andresmaz0 <andresdavidmazo@gmail.com>
2024-10-30 16:15:33 -0600

    Creating function validacion

--- Js/Medidas.js
+++ Js/Medidas.js
@@ -1,6 +1,14 @@
 const form = document.getElementById("form");
 
-let ancho = document.getElementById("ancho").value;
-let relacion = document.getElementById("relacion").value;
-let rin = document.getElementById("rin").value;
 let mensaje = document.getElementById("mensaje");
+
+let validacion = () => {
+    let ancho = document.getElementById("ancho").value;
+    let relacion = document.getElementById("relacion").value;
+    let rin = document.getElementById("rin").value;
+
+    mensaje.value='SE RECOMIENDA CAMBIAR DE MUESTRA';
+    mensaje.value='LA MUESTRA ES APTA PARA ESTE ENSAYO'
+}
+
+form.addEventListener("submit", )
