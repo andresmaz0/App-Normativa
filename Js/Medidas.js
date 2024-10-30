49b0b251d5be3fbc0c821bb6c2b22fae4698bd01
andresmaz0 <andresdavidmazo@gmail.com>
2024-10-30 16:09:46 -0600

    creating variables

---
+++ Js/Medidas.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form");
+
+let ancho = document.getElementById("ancho").value;
+let relacion = document.getElementById("relacion").value;
+let rin = document.getElementById("rin").value;
+let mensaje = document.getElementById("mensaje");
