d802ce9285893f41f75d1d6cd8489da54c68ef25
andresmaz0 <andresdavidmazo@gmail.com>
2024-10-31 12:03:40 -0600

    Creating and filling map

--- Js/Medidas.js
+++ Js/Medidas.js
@@ -1,7 +1,14 @@
 const form = document.getElementById("form");
-let datos_rin = [24,394,22,368,21,356,20,345,19,330,18,318,17,305,16,292,15,279,14,267,13,254,12,241,11,229,10,216];
-
 let mensaje = document.getElementById("mensaje");
+
+let datos_rin = [24,394,22,368,21,356,20,345,19,330,18,318,17,305,16,292,15,279,14,267,13,254,12,241,11,229,10,216];
+let map = new Map();
+
+for(let i=0;i<=datos_rin.length-1;i+=2){
+    map.set(datos_rin[i],datos_rin[i+1]);
+}
+console.log(map);
+
 
 let validacion = (e) => {
     //quitando el evento por defecto del navegador
