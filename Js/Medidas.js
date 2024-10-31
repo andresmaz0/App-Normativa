af5896f68d7b3cd36620460dc22f2fce8bc0af8e
andresmaz0 <andresdavidmazo@gmail.com>
2024-10-31 10:27:17 -0600

    Adding commet in preventDefault

--- Js/Medidas.js
+++ Js/Medidas.js
@@ -3,7 +3,9 @@ const form = document.getElementById("form");
 let mensaje = document.getElementById("mensaje");
 
 let validacion = (e) => {
+    //quitando el evento por defecto del navegador
     e.preventDefault();
+    
     let ancho = document.getElementById("ancho").value;
     let relacion = document.getElementById("relacion").value;
     let rin = document.getElementById("rin").value;
